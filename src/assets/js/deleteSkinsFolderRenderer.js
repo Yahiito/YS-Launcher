e1101858a5ede9d886db3bf0029420d618ce3add
Yahiito <semy.adel.38@gmail.com>
2025-07-15 16:53:14 +0200

    Publish 1.1.6

--- src/assets/js/deleteSkinsFolderRenderer.js
+++
@@ -1,6 +0,0 @@
-// Appelle directement la fonction de suppression du dossier skins
-const deleteSkinsFolder = require('../../../deleteSkinsFolder');
-
-window.deleteSkinsFolder = function() {
-    deleteSkinsFolder();
-};
